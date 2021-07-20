15bfc3df30eb30c9b30ae384f4b95ca4ed1f954d
bdarab <bdarabzand@gmail.com>
2021-07-20 23:31:15 +0200

    Add class calculator & its methods

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-const prevDisplay = document.querySelector('.previous-view');
-const currDisplay = document.querySelector('.current-view');
+class Calculator{
+  constructor(preScreen, currScreen){
+    this.preScreen = preScreen;
+    this.currScreen = currScreen;
+    this.clear()
+  }
+  clear(){
+    this.prevDispaly = ""
+    this.currDisplay = ""
+    this.operation = undefined
+  }
+  addNumberToScreen(number){
+this.currDisplay = number
+  }
+  updateScreen(){
+    this.currScreen.innerText = this.currDisplay
+  }
+  selectedOperation(operation){
+
+  }
+  calculate(){
+
+  }
+  delete(){
+
+  }
+}
+
+const prevScreen = document.querySelector('.previous-view');
+const currScreen = document.querySelector('.current-view');
 const numberBtn = document.querySelectorAll('[data-number]');
 const operationBtn = document.querySelectorAll('[data-operation]');
 const deleteBtn = document.querySelector('[data-delete]');
 const clearBtn = document.querySelector('[data-all-clear]');
 const equalBtn = document.querySelector('[data-equals]');
+
+const calculator = new Calculator(prevScreen, currScreen)
+
+numberBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.addNumberToScreen(button.innerText)
+    calculator.updateScreen()
+  })
+})
