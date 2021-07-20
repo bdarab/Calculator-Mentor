4727579c11ffa96eb5580c1dec35d5c7a5e3ed66
bdarab <bdarabzand@gmail.com>
2021-07-20 18:36:21 +0200

    Initialize Repository

---
+++ app.js
@@ -0,0 +1,7 @@
+const prevDisplay = document.querySelector('.previous-view');
+const currDisplay = document.querySelector('.current-view');
+const numberBtn = document.querySelectorAll('[data-number]');
+const operationBtn = document.querySelectorAll('[data-operation]');
+const deleteBtn = document.querySelector('[data-delete]');
+const clearBtn = document.querySelector('[data-all-clear]');
+const equalBtn = document.querySelector('[data-equals]');
